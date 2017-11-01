de80ca85f585269ffa229ca0b9cd09504c00b106
mathildell <mathildell@users.noreply.github.com>
2017-11-01 11:00:12 -0400

    Update DatabaseConfig.php

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -2,7 +2,7 @@
 class DB_CONFIG
 {
   const DB_NAME = 'MYSQL_DATABASE_NAME';
-  const DB_UN = 'root'; //'MYSQL_DATABASE_USERNAME';
-  const DB_PWD = 'root';//'MYSQL_DATABASE_PASSWORD';
+  const DB_UN = 'MYSQL_DATABASE_USERNAME';
+  const DB_PWD = 'MYSQL_DATABASE_PASSWORD';
   const DB_HOST = 'localhost';
 }
