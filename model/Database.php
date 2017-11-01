4a86608ee8143680dbfe9816043f6d494a40b735
mathildell <mathildell@users.noreply.github.com>
2017-11-01 11:13:57 -0400

    Update Database.php

--- model/Database.php
+++ model/Database.php
@@ -9,6 +9,7 @@ class Database{
   public function __construct(){
 
     if(!$this->db){
+      //PDO: http://php.net/manual/en/class.pdo.php
       $this->db = new PDO('mysql:host='.DB_CONFIG::DB_HOST.';dbname='.DB_CONFIG::DB_NAME.'', DB_CONFIG::DB_UN, DB_CONFIG::DB_PWD);
       $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
@@ -22,7 +23,7 @@ class Database{
   }
 
   //On peut ajouter les fonctions d'ajout, d'édition etc ci-après
-  //Réccuperer une seule column avec un id:
+  //Récuperer une seule column avec un id:
   //  SELECT * FROM :table WHERE id = :id;
   //Ajout: 
   //  INSERT INTO :table (:column_1, :column_2, :column_3) VALUES (:value_1, :value_2, :value_3);
